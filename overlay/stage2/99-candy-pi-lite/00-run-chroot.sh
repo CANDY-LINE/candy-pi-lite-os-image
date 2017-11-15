b62ba9c5ac98f81e12a70403741d732dff1395a1
Daisuke Baba <baba.daisuke@gmail.com>
2017-11-15 14:52:37 +0900

    Add a script file for CANDY Pi Lite installation at stage2

---
+++ overlay/stage2/99-candy-pi-lite/00-run-chroot.sh
@@ -0,0 +1,8 @@
+# on_chroot
+
+CANDY_PI_LITE_VERSION=master
+
+curl -sL \
+  https://raw.githubusercontent.com/CANDY-LINE/candy-pi-lite-service/${CANDY_PI_LITE_VERSION}/install.sh | \
+  ENABLE_WATCHDOG=1 \
+  bash

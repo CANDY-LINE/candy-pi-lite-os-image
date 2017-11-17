68dd937cda4d28d8d3540d30a3e12e478091e6ac
Daisuke Baba <baba.daisuke@gmail.com>
2017-11-17 16:28:42 +0900

    Fix an issue where GIT_HASH cannot be resolved within update_issue() function

--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -10,16 +10,29 @@ function update_sibmodules {
 function create_config {
   IMG_NAME=${2:-candy-pi-lite-raspbian}
   echo "IMG_NAME=${IMG_NAME}" > ${PIGEN_DIR}/config
+  pushd ${PIGEN_DIR}
+  GIT_HASH=$(git rev-parse HEAD)
+  popd
+  echo "GIT_HASH=${GIT_HASH}" >> ${PIGEN_DIR}/config
 }
 
 function configure_stages {
   touch ${PIGEN_DIR}/stage{3,4,5}/SKIP
-  rm -f ${PIGEN_DIR}/stage{3,4,5}/EXPORT_*
+  rm -f ${PIGEN_DIR}/stage{2,3,4,5}/EXPORT_NOOBS
+  rm -f ${PIGEN_DIR}/stage{2,3,4,5}/EXPORT_IMAGE
   pushd $(pwd)/overlay
-  cp -r stage* ../deps/pi-gen/
+  for STAGE in $(ls -d stage*); do
+    rm -fr ../deps/pi-gen/${STAGE}
+    cp -fr ${STAGE} ../deps/pi-gen/
+  done
   popd
+}
+
+function configure_scripts {
+  cp -f $(pwd)/scripts/common ${PIGEN_DIR}/scripts
 }
 
 update_sibmodules
 create_config
 configure_stages
+configure_scripts

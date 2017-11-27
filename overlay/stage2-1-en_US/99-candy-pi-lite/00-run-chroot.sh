03b7c9f01277360619ea613f42a75b145f7e4bff
Daisuke Baba <baba.daisuke@gmail.com>
2017-11-28 08:59:46 +0900

    Install all packages used in candy-pi-lite service and CANDY RED at once for faster installation

--- overlay/stage2-1-en_US/99-candy-pi-lite/00-run-chroot.sh
+++ overlay/stage2-1-en_US/99-candy-pi-lite/00-run-chroot.sh
@@ -1,8 +1,14 @@
 # on_chroot
 
+# CANDY Pi Lite and CANDY RED
+
 CANDY_PI_LITE_VERSION=master
 
 curl -sL \
   https://raw.githubusercontent.com/CANDY-LINE/candy-pi-lite-service/${CANDY_PI_LITE_VERSION}/install.sh | \
   FORCE_INSTALL=1 \
+  CANDY_PI_LITE_APT_GET_UPDATED=1 \
   bash
+
+# Sense HAT node
+pip install pillow

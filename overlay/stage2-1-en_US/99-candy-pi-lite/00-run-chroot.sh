7c4c0d7d7a520b2d58be81f123d780b85dc36b02
Daisuke Baba <baba.daisuke@gmail.com>
2017-12-05 19:11:02 +0900

    Provide the specific board type

--- overlay/stage2-1-en_US/99-candy-pi-lite/00-run-chroot.sh
+++ overlay/stage2-1-en_US/99-candy-pi-lite/00-run-chroot.sh
@@ -6,6 +6,7 @@ CANDY_PI_LITE_VERSION=master
 
 curl -sL \
   https://raw.githubusercontent.com/CANDY-LINE/candy-pi-lite-service/${CANDY_PI_LITE_VERSION}/install.sh | \
+  BOARD=RPi \
   FORCE_INSTALL=1 \
   CANDY_PI_LITE_APT_GET_UPDATED=1 \
   bash

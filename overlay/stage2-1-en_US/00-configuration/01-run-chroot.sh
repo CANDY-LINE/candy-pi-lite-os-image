f3d38e0b5f08bb0b0503e3639b026e8ff00b84de
Daisuke Baba <baba.daisuke@gmail.com>
2019-11-15 11:40:47 +0900

    Bump Node.js version

--- overlay/stage2-1-en_US/00-configuration/01-run-chroot.sh
+++ overlay/stage2-1-en_US/00-configuration/01-run-chroot.sh
@@ -5,7 +5,7 @@ ln -s /usr/share/zoneinfo/UTC /etc/localtime
 
 locale-gen
 
-ARMv6_NODEJS_VERSION="8.16.0"
+ARMv6_NODEJS_VERSION="10.15.3"
 
 apt-get remove -y nodered nodejs nodejs-legacy npm
 rm -f \

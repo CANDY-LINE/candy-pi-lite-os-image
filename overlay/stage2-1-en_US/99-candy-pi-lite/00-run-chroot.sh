211b8f63250e6bf078c35f84114c607718c0af80
Daisuke Baba <baba.daisuke@gmail.com>
2019-11-18 09:53:07 +0900

    Fix SSL error on curl

--- overlay/stage2-1-en_US/99-candy-pi-lite/00-run-chroot.sh
+++ overlay/stage2-1-en_US/99-candy-pi-lite/00-run-chroot.sh
@@ -4,12 +4,9 @@
 
 CANDY_PI_LITE_VERSION=master
 
-curl -sL \
-  https://raw.githubusercontent.com/CANDY-LINE/candy-pi-lite-service/${CANDY_PI_LITE_VERSION}/install.sh | \
-  BOARD=RPi \
-  FORCE_INSTALL=1 \
-  CANDY_PI_LITE_APT_GET_UPDATED=1 \
-  bash
+# Workaround for the SSL error
+export SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
+curl -vsL https://raw.githubusercontent.com/CANDY-LINE/candy-pi-lite-service/${CANDY_PI_LITE_VERSION}/install.sh | BOARD=RPi FORCE_INSTALL=1 CANDY_PI_LITE_APT_GET_UPDATED=1 bash
 
 # Always Enable UFW
 sed -i -e "s/ENABLED=no/ENABLED=yes/g" /etc/ufw/ufw.conf

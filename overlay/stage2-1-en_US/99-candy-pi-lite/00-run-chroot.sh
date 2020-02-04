d81394c86381029fbe1ecceea8f60f9f58a7a36f
Daisuke Baba <baba.daisuke@gmail.com>
2020-02-04 15:51:32 +0900

    Introduce a new variable to install systemd

--- overlay/stage2-1-en_US/99-candy-pi-lite/00-run-chroot.sh
+++ overlay/stage2-1-en_US/99-candy-pi-lite/00-run-chroot.sh
@@ -10,7 +10,7 @@ if [ "${CANDY_RED_HASH}" = "latest" ]; then
 else
     # CANDY RED@${CANDY_RED_HASH}
     curl -vsL https://raw.githubusercontent.com/CANDY-LINE/candy-pi-lite-service/${CANDY_PI_LITE_VERSION}/install.sh | CANDY_RED=0 BOARD=RPi FORCE_INSTALL=1 CANDY_PI_LITE_APT_GET_UPDATED=1 bash
-    CANDY_RED_APT_GET_UPDATED=1 npm install -g --unsafe-perm --production https://github.com/CANDY-LINE/candy-red/archive/${CANDY_RED_HASH}.tar.gz
+    CANDY_RED_APT_GET_UPDATED=1 LOCAL_INSTALL=0 npm install -g --unsafe-perm --production https://github.com/CANDY-LINE/candy-red/archive/${CANDY_RED_HASH}.tar.gz
 fi
 
 # Always Enable UFW

cbc063e8bfbdc058b93a0eb3fdb74127af1ded2d
Daisuke Baba <baba.daisuke@gmail.com>
2020-02-04 18:54:27 +0900

    Add build commands as the tarball doesn't have dist stuff

--- overlay/stage2-1-en_US/99-candy-pi-lite/00-run-chroot.sh
+++ overlay/stage2-1-en_US/99-candy-pi-lite/00-run-chroot.sh
@@ -8,9 +8,13 @@ CANDY_RED_HASH=latest
 if [ "${CANDY_RED_HASH}" = "latest" ]; then
     curl -vsL https://raw.githubusercontent.com/CANDY-LINE/candy-pi-lite-service/${CANDY_PI_LITE_VERSION}/install.sh | BOARD=RPi FORCE_INSTALL=1 CANDY_PI_LITE_APT_GET_UPDATED=1 bash
 else
-    # CANDY RED@${CANDY_RED_HASH}
+    # CANDY RED@${CANDY_RED_HASH} with development dependencies
     curl -vsL https://raw.githubusercontent.com/CANDY-LINE/candy-pi-lite-service/${CANDY_PI_LITE_VERSION}/install.sh | CANDY_RED=0 BOARD=RPi FORCE_INSTALL=1 CANDY_PI_LITE_APT_GET_UPDATED=1 bash
     CANDY_RED_APT_GET_UPDATED=1 LOCAL_INSTALL=0 npm install -g --unsafe-perm --production https://github.com/CANDY-LINE/candy-red/archive/${CANDY_RED_HASH}.tar.gz
+    pushd /usr/lib/node_modules/candy-red
+    DEVEL=true npm install --unsafe-perm
+    npm run build
+    popd
 fi
 
 # Always Enable UFW

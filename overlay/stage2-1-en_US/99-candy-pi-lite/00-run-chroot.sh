cd7c025af15cb6405d3a1a055e4c7e074d52ff70
Daisuke Baba <baba.daisuke@gmail.com>
2020-02-29 13:42:08 +0900

    Exit the process with error status code on error

--- overlay/stage2-1-en_US/99-candy-pi-lite/00-run-chroot.sh
+++ overlay/stage2-1-en_US/99-candy-pi-lite/00-run-chroot.sh
@@ -8,10 +8,22 @@ BOOT_APN=iijmobile.biz-ipv4v6
 
 if [ "${CANDY_RED_HASH}" = "latest" ]; then
     curl -vsL https://raw.githubusercontent.com/CANDY-LINE/candy-pi-lite-service/${CANDY_PI_LITE_VERSION}/install.sh | BOOT_APN="${BOOT_APN}" BOARD=RPi FORCE_INSTALL=1 CANDY_PI_LITE_APT_GET_UPDATED=1 bash
+    if [ "$?" != "0" ]; then
+        echo "FAILED TO INSTALL candy-pi-lite-service@${CANDY_PI_LITE_VERSION}"
+        exit 1
+    fi
 else
     # CANDY RED@${CANDY_RED_HASH} with development dependencies
     curl -vsL https://raw.githubusercontent.com/CANDY-LINE/candy-pi-lite-service/${CANDY_PI_LITE_VERSION}/install.sh | BOOT_APN="${BOOT_APN}" CANDY_RED=0 BOARD=RPi FORCE_INSTALL=1 CANDY_PI_LITE_APT_GET_UPDATED=1 bash
+    if [ "$?" != "0" ]; then
+        echo "FAILED TO INSTALL candy-pi-lite-service@${CANDY_PI_LITE_VERSION}"
+        exit 1
+    fi
     CANDY_RED_APT_GET_UPDATED=1 LOCAL_INSTALL=0 npm install -g --unsafe-perm --production https://github.com/CANDY-LINE/candy-red/archive/${CANDY_RED_HASH}.tar.gz
+    if [ "$?" != "0" ]; then
+        echo "FAILED TO INSTALL candy-red@${CANDY_RED_HASH}"
+        exit 1
+    fi
     pushd /usr/lib/node_modules/candy-red
     # Install all dependencies and run prepare stage script
     LOCAL_INSTALL=0 DEVEL=true npm install --unsafe-perm

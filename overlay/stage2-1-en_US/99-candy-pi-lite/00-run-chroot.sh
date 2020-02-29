52088a60f3af265920018322343d470a5c5741cd
Daisuke Baba <baba.daisuke@gmail.com>
2020-02-29 10:46:11 +0900

    Fix a dependency error

--- overlay/stage2-1-en_US/99-candy-pi-lite/00-run-chroot.sh
+++ overlay/stage2-1-en_US/99-candy-pi-lite/00-run-chroot.sh
@@ -12,8 +12,8 @@ else
     curl -vsL https://raw.githubusercontent.com/CANDY-LINE/candy-pi-lite-service/${CANDY_PI_LITE_VERSION}/install.sh | BOOT_APN="${BOOT_APN}" CANDY_RED=0 BOARD=RPi FORCE_INSTALL=1 CANDY_PI_LITE_APT_GET_UPDATED=1 bash
     CANDY_RED_APT_GET_UPDATED=1 LOCAL_INSTALL=0 npm install -g --unsafe-perm --production https://github.com/CANDY-LINE/candy-red/archive/${CANDY_RED_HASH}.tar.gz
     pushd /usr/lib/node_modules/candy-red
-    # Install devDependencies and run prepare stage script
-    LOCAL_INSTALL=0 DEVEL=true npm install --only=dev --unsafe-perm
+    # Install all dependencies and run prepare stage script
+    LOCAL_INSTALL=0 DEVEL=true npm install --unsafe-perm
     popd
 fi
 

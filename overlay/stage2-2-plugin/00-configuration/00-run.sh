4234c347b7fe6bdbb9a6bf0162470451cfca5a41
Daisuke Baba <baba.daisuke@gmail.com>
2020-03-28 15:10:16 +0900

    Strip custom scripts

--- overlay/stage2-2-plugin/00-configuration/00-run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-mv ${ROOTFS_DIR}/usr/lib/node_modules/candy-red/services/systemd/environment ${ROOTFS_DIR}/usr/lib/node_modules/candy-red/services/systemd/environment.bak
-install -v -m 600 files/environment ${ROOTFS_DIR}/usr/lib/node_modules/candy-red/services/systemd/
-
-install -v -m 644 files/plugin.tgz ${ROOTFS_DIR}/usr/lib/node_modules/candy-red/plugin.tgz
--- overlay/stage2-2-plugin/01-install-plugin/00-run-chroot.sh
+++
@@ -1,8 +0,0 @@
-# on_chroot
-
-# Install CANDY RED Plugin module
-
-pushd /usr/lib/node_modules/candy-red
-npm install --unsafe-perm ./plugin.tgz
-rm -f ./plugin.tgz
-popd
